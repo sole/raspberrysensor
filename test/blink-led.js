a586d4eb5e7ebbab728a24b024a97f43799265c6
sole <listas@soledadpenades.com>
2020-11-08 18:36:16 +0000

    add simple test to see if this works

---
+++ test/blink-led.js
@@ -0,0 +1,11 @@
+const { RaspiIO } = require('raspi-io');
+const five = require('johnny-five');
+const board = new five.Board({
+  io: new RaspiIO()
+});
+
+board.on('ready', () => {
+  // Create an Led on pin 7 on header P1 (GPIO4) and strobe it on/off
+  const led = new five.Led('P1-7');
+  led.strobe(500);
+});
