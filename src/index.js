bd76a429eed3c0d9e806eb65d10d96a987f5c483
sole <listas@soledadpenades.com>
2020-11-12 21:49:28 +0000

    add support for static assets

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
+const WEBSITE_PATH = path.join(__dirname, 'website');
 
+app.use(express.static(WEBSITE_PATH));
+	
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
